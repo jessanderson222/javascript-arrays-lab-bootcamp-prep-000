abf6b95671d0833ae806bea197b3bd6bc8f8bb7f
JESSIE ANDERSON <jessanderson222@gmail.com>
2018-09-05 22:57:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  var kittens = ["Milo", "Otis"]
+  var kittens = ["Milo", "Otis", "Garfield"]
   return kittens.push(name)
 }
 destructivelyAppendKitten("Ralph")
