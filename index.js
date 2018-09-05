1070c52ce9ddd48ebd06c2cffbeb8cd8234ef8ca
JESSIE ANDERSON <jessanderson222@gmail.com>
2018-09-05 23:03:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function destructivelyAppendKitten(name) {
   return kittens.push(name)
 }
 destructivelyAppendKitten("Ralph")
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+destructivelyPrependKitten("Ralph")
